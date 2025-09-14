b211d7193892e68438922ec7b01cb548a16bd8fd
bonaparks-dev <bonaparks@gmail.com>
2025-09-14 07:19:04 -0700

    Update EnvGuard.tsx

--- components/system/EnvGuard.tsx
+++ components/system/EnvGuard.tsx
@@ -1,4 +1,4 @@
-envguard.tsx import React from 'react';
+import React from 'react';
 
 const EnvGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => { // The environment variables have been hardcoded to resolve a configuration issue. // This guard is now disabled and simply renders its children. return <>{children}</>; };
 
