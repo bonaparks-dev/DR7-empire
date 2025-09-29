7236ca6900821515a77b9fd6142890317cd3e704
bonaparks-dev <bonaparks@gmail.com>
2025-09-29 16:30:35 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ import RentalPage from './pages/RentalPage';
 import MembershipPage from './pages/MembershipPage';
 import { RENTAL_CATEGORIES } from './constants';
 import { AnimatePresence, motion } from 'framer-motion';
-import BookingModal from './components/ui/BookingModal';
+import CarBookingWizard from './components/ui/CarBookingWizard';
 import BookingPage from './pages/BookingPage';
 import CommercialOperationPage from './pages/CommercialOperationPage';
 import AboutPage from './pages/AboutPage';
