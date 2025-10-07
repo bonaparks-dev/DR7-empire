1e217a6e595c2cd7e8ec713dc3d2aef16af512eb
bonaparks-dev <bonaparks@gmail.com>
2025-10-08 00:58:06 +0200

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -468,7 +468,7 @@ export const COMMERCIAL_OPERATION_GIVEAWAY: CommercialOperation = {
   subtitle: { en: 'Prize Pool Worth Over €7,000,000', it: 'Montepremi di Oltre €7.000.000' },
   image: '/main.jpeg',
   ticketPriceUSD: 23.60,
-  ticketPriceEUR: 1,
+  ticketPriceEUR: 20,
   drawDate: '2025-12-24T10:00:00Z',
   prizes: [
     {
