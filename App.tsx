2e3e207f04deafb587175247b56f29a6c7d7a1d6
Ophelie Giraud <opheliegiraud@Ophelies-MacBook-Pro.local>
2026-01-19 16:11:21 +0100

    fix: Enable payment success and cancel page routes

--- App.tsx
+++ App.tsx
@@ -213,8 +213,8 @@ const AnimatedRoutes = () => {
         <Route path="/confirmation-success" element={<ConfirmationSuccessPage />} />
         <Route path="/car-booking-success" element={<CarBookingConfirmationPage />} />
         <Route path="/booking-success" element={<ConfirmationSuccessPage />} />
-        {/* <Route path="/payment-success" element={<PaymentSuccessPage />} /> */}
-        {/* <Route path="/payment-cancel" element={<PaymentCancelPage />} /> */}
+        <Route path="/payment-success" element={<PaymentSuccessPage />} />
+        <Route path="/payment-cancel" element={<PaymentCancelPage />} />
         <Route path="/post/:id" element={<PostPage />} />
       </Routes>
     </AnimatePresence>
